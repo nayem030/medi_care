e99f8c975507e5f9e3a9d43b644f3c87d2fb379f
Nayem Sheikh <naimesheikh.ns@gmail.com>
2024-05-11 15:50:29 +0600

    medical_fee

--- medical_packages/models.py
+++ medical_packages/models.py
@@ -4,6 +4,7 @@ class MedicalPackage(models.Model):
     name=models.CharField(max_length=50)
     descriptions=models.TextField()
     image=models.ImageField(upload_to="medical_package/images/")
+    fee=models.IntegerField(null=True)
     
     
     def __str__(self):
