4caadba1fe0e375c418c7eae92895c0e2c112515
Nayem Sheikh <naimesheikh.ns@gmail.com>
2024-05-15 16:07:36 +0600

    change_appointment

--- appointment/models.py
+++ appointment/models.py
@@ -6,7 +6,7 @@ from patient.models import Patient
 class Appointment(models.Model):
     name=models.CharField(max_length=50)
     email=models.EmailField(max_length=100)
-    doctor= models.CharField(max_length=100)
+    doctor= models.ForeignKey(Doctor, on_delete=models.CASCADE)
     department=models.ForeignKey(Department, on_delete=models.CASCADE)
     date=models.DateField()
     time=models.TimeField()
