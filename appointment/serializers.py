1d0c01e3ececdc40e3bddeb61b49d7c0b7cd1239
Nayem Sheikh <naimesheikh.ns@gmail.com>
2024-05-15 16:47:35 +0600

    aaaaaaaaaaaa

--- appointment/serializers.py
+++ appointment/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .import models
 
 class AppointmentSerializers(serializers.ModelSerializer):
-    department= serializers.StringRelatedField(many=False)
-    doctor= serializers.StringRelatedField(many=False)
+    department= serializers.StringRelatedField(many=True)
+    doctor= serializers.StringRelatedField(many=True)
     class Meta:
         model=models.Appointment
         fields ='__all__'
